820d45aff8c56c06b6b31bc002cbc125f2298cd0
Your Name <michaelwaruiru@gmail.com>
2025-01-27 23:52:38 +0300

    Deleted unnecessary codes

--- app/utils/mpesa.py
+++ app/utils/mpesa.py
@@ -20,7 +20,6 @@ def get_access_token():
     auth_url = f"{BASE_URL}/oauth/v1/generate?grant_type=client_credentials"
     response = requests.get(auth_url, auth=(CONSUMER_KEY, CONSUMER_SECRET))
     response_data = response.json()
-    print(f"MPesa Access Token: {response_data}")
     return response_data['access_token']
 
 def initiate_payment(phone_number, amount):
@@ -47,8 +46,6 @@ def initiate_payment(phone_number, amount):
         "AccountReference": "SubscriptionPayment",
         "TransactionDesc": "Payment for monthly bot subscription"
     }
-    
-    print("MPesa Payload:", payload)
 
     stk_url = f"{BASE_URL}/mpesa/stkpush/v1/processrequest"
     response = requests.post(stk_url, json=payload, headers=headers)
