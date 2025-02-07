150bac319e21a00bc272da1598e82a10c6bf51ce
Your Name <michaelwaruiru@gmail.com>
2025-02-07 19:52:00 +0300

    Changed load_dotenv location

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,9 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_mysqldb import MySQL
-from dotenv import load_dotenv
-import os
 from flask_cors import CORS
 from flask_mail import Mail
 from app.config import Config
-
-load_dotenv()
 
 mysql = MySQL()
 bcrypt = Bcrypt()
